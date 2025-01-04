c75b8cff44c9c47bb21dbaf94dc4a76bebbf24ec
Indra Tristia <tristia71@gmail.com>
2025-01-04 15:51:57 +0700

    Add config call component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,11 @@
-import About from "@/components/organism/About";
-import Footer from "@/components/organism/Footer";
-import Header from "@/components/organism/Headers";
-import Hero from "@/components/organism/Hero";
-import Portfolio from "@/components/organism/Portfolio";
-import Project from "@/components/organism/Project";
+import Main from "@/components/Pages/Main";
 import { ThemeProvider } from "next-themes";
 
 export default function Home() {
   return (
     <>
       <ThemeProvider attribute="class">
-        <Header />
-        <Hero />
-        <About />
-        <Portfolio />
-        {/* <Timeline /> */}
-        <Project />
-        <Footer />
+        <Main />
       </ThemeProvider>
     </>
   );
