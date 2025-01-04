ee218a50f99cc52e938106497482992043d2b9af
Indra Tristia <tristia71@gmail.com>
2025-01-04 13:54:43 +0700

    add : read data from json

--- src/components/molecules/Card/CardProject.tsx
+++ src/components/molecules/Card/CardProject.tsx
@@ -4,10 +4,8 @@ import React from "react";
 import CategoryStack from "../CategoryStack";
 interface ProjectCardProps {
   project: ProjectType;
-  isGrid?: boolean;
 }
 const CardProject: React.FC<ProjectCardProps> = ({ project }) => {
-  console.log(project.image);
   return (
     <>
       <div className="project-card">
@@ -21,54 +19,12 @@ const CardProject: React.FC<ProjectCardProps> = ({ project }) => {
             </h5>
           </a>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            {/* Salah satu hasil explore dari youtube yang menggunakan tailwind
-            namun diproject ini diubah menjadi hanya menggunakan Html,Css dan
-            sedikit Javascript. */}
             {project.description}
           </p>
           <h4 className="font-semibold mt-3 text-gray-700 dark:text-gray-400">
             Tech Stack :
           </h4>
           <div className="flex gap-x-2 flex-wrap">
-            {/* <span className="inline-flex items-center  px-3 py-2 mt-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
-              <svg
-                role="img"
-                width="22px"
-                className="fill-current"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <title>HTML5</title>
-                <path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.564-2.438L1.5 0zm7.031 9.75l-.232-2.718 10.059.003.23-2.622L5.412 4.41l.698 8.01h9.126l-.326 3.426-2.91.804-2.955-.81-.188-2.11H6.248l.33 4.171L12 19.351l5.379-1.443.744-8.157H8.531z" />
-              </svg>
-              &nbsp;Html
-            </span>
-            <span className="inline-flex items-center px-3 py-2 mt-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-              <svg
-                role="img"
-                width="22px"
-                className="fill-current"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <title>CSS3</title>
-                <path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z" />
-              </svg>
-              &nbsp;Css
-            </span>
-            <span className="inline-flex items-center px-3 py-2 mt-2 text-sm font-medium text-center text-white bg-yellow-700 rounded-lg hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">
-              <svg
-                role="img"
-                width="22px"
-                className="fill-current"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <title>JavaScript</title>
-                <path d="M0 0h24v24H0V0zm22.034 18.276c-.175-1.095-.888-2.015-3.003-2.873-.736-.345-1.554-.585-1.797-1.14-.091-.33-.105-.51-.046-.705.15-.646.915-.84 1.515-.66.39.12.75.42.976.9 1.034-.676 1.034-.676 1.755-1.125-.27-.42-.404-.601-.586-.78-.63-.705-1.469-1.065-2.834-1.034l-.705.089c-.676.165-1.32.525-1.71 1.005-1.14 1.291-.811 3.541.569 4.471 1.365 1.02 3.361 1.244 3.616 2.205.24 1.17-.87 1.545-1.966 1.41-.811-.18-1.26-.586-1.755-1.336l-1.83 1.051c.21.48.45.689.81 1.109 1.74 1.756 6.09 1.666 6.871-1.004.029-.09.24-.705.074-1.65l.046.067zm-8.983-7.245h-2.248c0 1.938-.009 3.864-.009 5.805 0 1.232.063 2.363-.138 2.711-.33.689-1.18.601-1.566.48-.396-.196-.597-.466-.83-.855-.063-.105-.11-.196-.127-.196l-1.825 1.125c.305.63.75 1.172 1.324 1.517.855.51 2.004.675 3.207.405.783-.226 1.458-.691 1.811-1.411.51-.93.402-2.07.397-3.346.012-2.054 0-4.109 0-6.179l.004-.056z" />
-              </svg>
-              &nbsp;JavaScript
-            </span> */}
             <CategoryStack categories={project.categories} />
           </div>
         </div>
