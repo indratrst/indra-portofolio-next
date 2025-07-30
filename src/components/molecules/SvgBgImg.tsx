821903d2da0c6489ffdd3849d3a84bcd91f9797e
Indra Tristia <tristia71@gmail.com>
2025-07-30 17:11:31 +0700

    Add Svg

---
+++ src/components/molecules/SvgBgImg.tsx
@@ -0,0 +1,88 @@
+const SvgEducation = () => {
+  return (
+    <div className=" ">
+      <svg
+        width="344"
+        height="364"
+        viewBox="0 0 344 364"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          cx="167.301"
+          cy="164.301"
+          r="151.85"
+          stroke="white"
+          stroke-width="6.90229"
+          stroke-dasharray="51.77 51.77"
+        />
+        <g filter="url(#filter0_d_4621_19)">
+          <mask
+            id="mask0_4621_19"
+            style="mask-type:alpha"
+            maskUnits="userSpaceOnUse"
+            x="36"
+            y="24"
+            width="261"
+            height="281"
+          >
+            <ellipse
+              cx="166.438"
+              cy="164.721"
+              rx="130.166"
+              ry="140.191"
+              fill="white"
+            />
+          </mask>
+          <g mask="url(#mask0_4621_19)">
+            <ellipse
+              cx="166.438"
+              cy="164.437"
+              rx="130.166"
+              ry="141.077"
+              fill="#E9EAF6"
+            />
+          </g>
+        </g>
+        <defs>
+          <filter
+            id="filter0_d_4621_19"
+            x="0.0356908"
+            y="0.372143"
+            width="343.159"
+            height="363.21"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx="5.17672" dy="17.2557" />
+            <feGaussianBlur stdDeviation="20.7069" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0.160267 0 0 0 0 0.169777 0 0 0 0 0.420833 0 0 0 0.3 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow_4621_19"
+            />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="effect1_dropShadow_4621_19"
+              result="shape"
+            />
+          </filter>
+        </defs>
+      </svg>
+    </div>
+  );
+};
+
+export default SvgEducation;
