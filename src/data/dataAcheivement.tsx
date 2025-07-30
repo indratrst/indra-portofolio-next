aafba82fead00abae36d4f2cdd3b745937600d59
Indra Tristia <tristia71@gmail.com>
2025-07-30 21:26:50 +0700

    add data achievement

---
+++ src/data/dataAcheivement.tsx
@@ -0,0 +1,85 @@
+import { AcheivementType } from "./type";
+
+export const acheivementData: AcheivementType[] = [
+  {
+    image: "/sertifikat/jago-lev2.png",
+    title:
+      "Bootcamp Intensif Level 2 Development With Framework [React Js,Laravel,Mysql]",
+    description:
+      "AI-powered Virtual Assistant that swiftly answers queries via text or image prompts",
+  },
+  {
+    image: "/sertifikat/jago-lev1.png",
+    title: "Bootcamp Intensif Level 1 Fundamental Programming",
+    description:
+      "AI-powered Virtual Assistant that swiftly answers queries via text or image prompts",
+  },
+  {
+    image: "/sertifikat/frontend-expert.png",
+    title: "Kelas Front End Web Developer [Begginer - Intermediate]",
+    description:
+      "AI-powered Virtual Assistant that swiftly answers queries via text or image prompts",
+  },
+  {
+    image: "/sertifikat/fundamental-fe.png",
+    title: "Kelas Front End Web Developer [Begginer - Intermediate]",
+    description:
+      "AI-powered Virtual Assistant that swiftly answers queries via text or image prompts",
+  },
+  {
+    image: "/sertifikat/frontend-pemula.png",
+    title: "Kelas Front End Web Developer [Beginner]",
+    description:
+      "AI-powered Virtual Assistant that swiftly answers queries via text or image prompts",
+  },
+  {
+    image: "/sertifikat/dasar-web.png",
+    title: "Kelas Front End Web Developer [Basic - Beginner]",
+    description:
+      "AI-powered Virtual Assistant that swiftly answers queries via text or image prompts",
+  },
+  {
+    image: "/sertifikat/dasar-js.png",
+    title: "Kelas Dasar Javascript",
+    description:
+      "AI-powered Virtual Assistant that swiftly answers queries via text or image prompts",
+  },
+
+  {
+    image: "/sertifikat/aws.png",
+    title: "Kelas Dasar Aws Cloud",
+    description:
+      "AI-powered Virtual Assistant that swiftly answers queries via text or image prompts",
+  },
+
+  {
+    image: "/sertifikat/backend-pemula.png",
+    title: "Kelas Backend Pemula",
+    description:
+      "AI-powered Virtual Assistant that swiftly answers queries via text or image prompts",
+  },
+  {
+    image: "/sertifikat/bdd.png",
+    title: "Event Baparekraf Developer Day 2023",
+    description:
+      "AI-powered Virtual Assistant that swiftly answers queries via text or image prompts",
+  },
+  {
+    image: "/sertifikat/dicoding-developer.jpg",
+    title: "Event Dicoding Developer Conference 2023",
+    description:
+      "AI-powered Virtual Assistant that swiftly answers queries via text or image prompts",
+  },
+  {
+    image: "/sertifikat/niagahoster.png",
+    title: "Webinar Fundamental Front End Web Development",
+    description:
+      "AI-powered Virtual Assistant that swiftly answers queries via text or image prompts",
+  },
+  {
+    image: "/sertifikat/english-2020.png",
+    title: "Kelas Belajar Bahasa Inggris - Sekolah Pintar",
+    description:
+      "AI-powered Virtual Assistant that swiftly answers queries via text or image prompts",
+  },
+];
