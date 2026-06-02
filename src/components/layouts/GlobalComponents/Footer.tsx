681868261dc755b3d5b8a07f8c232935d460bf85
Indra Tristia <tristia71@gmail.com>
2026-06-02 18:04:57 +0700

    add : icon

--- src/components/layouts/GlobalComponents/Footer.tsx
+++ src/components/layouts/GlobalComponents/Footer.tsx
@@ -2,26 +2,23 @@
 
 import Link from "next/link";
 import { motion } from "framer-motion";
-import {
-  Mail,
-  ArrowUpRight,
-} from "lucide-react";
+import { Mail, ArrowUpRight } from "lucide-react";
 
 const links = [
   {
     name: "Github",
-    href: "https://github.com/yourgithub",
-    // icon: Github,
+    href: "https://github.com/indratrst/",
+    icon: "M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12",
   },
   {
     name: "LinkedIn",
-    href: "https://linkedin.com/in/yourlinkedin",
-    // icon: Linkedin,
+    href: "https://www.linkedin.com/in/indra-tristia/",
+    icon: "M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z",
   },
   {
     name: "Email",
-    href: "mailto:your@email.com",
-    icon: Mail,
+    href: "mailto:indratrst@email.com",
+    icon: "M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z",
   },
 ];
 
@@ -56,22 +53,28 @@ export default function Footer() {
               Available for Opportunities
             </span>
 
-            <h2 className="mt-8 text-5xl font-bold tracking-tight text-white md:text-7xl">
-              Let's Build
+            <h2 className="mt-8 text-5xl font-bold tracking-tight text-white md:text-7xl ">
+              Let&apos;s Build
               <br />
-
               <span className="bg-gradient-to-r from-blue-700 to-red-700 bg-clip-text text-transparent">
                 Something Great
               </span>
-
               <br />
-              Together.
+              <span
+                className=" bg-gradient-to-br
+      from-white
+      via-white
+      to-[#077fff42]
+      bg-clip-text
+      text-transparent"
+              >
+                Together.
+              </span>
             </h2>
 
             <p className="mt-8 max-w-2xl text-lg text-zinc-400">
-              Frontend Developer specializing in React,
-              Next.js, TypeScript and modern web
-              experiences.
+              Frontend Developer specializing in React, Next.js, TypeScript and
+              modern web experiences.
             </p>
           </motion.div>
 
@@ -103,9 +106,18 @@ export default function Footer() {
                     duration-300
                     hover:border-white/20
                     hover:bg-white/[0.05]
+                    z-20
                   "
                 >
-                  {/* <Icon size={18} /> */}
+                  <svg
+                    role="img"
+                    className="fill-current"
+                    width="20"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d={link.icon} />
+                  </svg>
 
                   <span>{link.name}</span>
 
@@ -126,7 +138,7 @@ export default function Footer() {
 
         {/* Massive Brand */}
 
-        <div className="absolute bottom-0 md:right-36 border-t border-white/10 py-16">
+        <div className="absolute bottom-0 md:right-36 md:border-t border-white/10 py-16">
           <div
             className="
               select-none
@@ -172,7 +184,7 @@ export default function Footer() {
             items-center
             justify-between
             gap-4
-            border-t
+            md:border-t
             border-white/10
             py-8
             text-sm
@@ -181,14 +193,10 @@ export default function Footer() {
           "
         >
           <p>
-            © {new Date().getFullYear()} Indra Tristia.
-            All rights reserved.
+            © {new Date().getFullYear()} Indra Tristia. All rights reserved.
           </p>
 
-          <p>
-            Built with Next.js, TypeScript &
-            Tailwind CSS
-          </p>
+          <p>Built with Next.js, TypeScript & Tailwind CSS</p>
         </div>
       </div>
     </footer>
