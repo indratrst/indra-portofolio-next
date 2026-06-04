a26bf465485c34d186ffb06a8ebf8de883e84e8a
Indra Tristia <tristia71@gmail.com>
2026-06-04 12:36:05 +0700

    update : translating to english at project section

--- src/data/dataProject.tsx
+++ src/data/dataProject.tsx
@@ -56,12 +56,38 @@ export const projectData: ProjectType[] = [
   //   github: "https://github.com/tangguhriyadi/chatbot-rag",
   // },
 
+  // 2026 UPDATE
+  // {
+  //   image: "/myproject/mdl.jpeg",
+  //   categories: ["Vue", "Tailwind", "javascript"],
+  //   title: "Mydigilearn",
+  //   description:
+  //     "Berperan sebagai Frontend Developer dalam membangun platform corporate learning berbasis Vue.js. Mengembangkan fitur berbagi konten dan modul pembelajaran interaktif khususnya untuk industri digital.",
+  //   link: "https://mydigilearn.id/",
+  // },
+  // {
+  //   image: "/myproject/learnhub.png",
+  //   categories: ["react", "next", "Tailwind", "typescript"],
+  //   title: "Learnhub",
+  //   description:
+  //     "Terlibat sebagai Frontend Developer dalam pengembangan website builder yang memungkinkan konten kreator, lembaga, dan komunitas membangun akademi online mereka sendiri tanpa coding, menggunakan React.js dan Next.js.",
+  //   link: "https://learnhub.id/",
+  // },
+  // {
+  //   image: "/myproject/torado.png",
+  //   categories: ["react", "Tailwind", "javascript"],
+  //   title: "Torado",
+  //   description:
+  //     "Membangun website company profile untuk Torado Group, perusahaan di bidang food and beverage, dengan fokus pada tampilan yang menarik, responsif, dan mencerminkan identitas brand.",
+  //   link: "https://toradogroup.com/",
+  // },
+  // ];
   {
     image: "/myproject/mdl.jpeg",
     categories: ["Vue", "Tailwind", "javascript"],
     title: "Mydigilearn",
     description:
-      "Berperan sebagai Frontend Developer dalam membangun platform corporate learning berbasis Vue.js. Mengembangkan fitur berbagi konten dan modul pembelajaran interaktif khususnya untuk industri digital.",
+      "Worked as a Frontend Developer on a Vue.js-based corporate learning platform. Responsible for developing content-sharing features and interactive learning modules that enhanced the digital learning experience.",
     link: "https://mydigilearn.id/",
   },
   {
@@ -69,7 +95,7 @@ export const projectData: ProjectType[] = [
     categories: ["react", "next", "Tailwind", "typescript"],
     title: "Learnhub",
     description:
-      "Terlibat sebagai Frontend Developer dalam pengembangan website builder yang memungkinkan konten kreator, lembaga, dan komunitas membangun akademi online mereka sendiri tanpa coding, menggunakan React.js dan Next.js.",
+      "Developed frontend features for a website builder platform using React.js and Next.js, enabling content creators, institutions, and communities to build and customize their own online learning academies without requiring coding skills.",
     link: "https://learnhub.id/",
   },
   {
@@ -77,7 +103,7 @@ export const projectData: ProjectType[] = [
     categories: ["react", "Tailwind", "javascript"],
     title: "Torado",
     description:
-      "Membangun website company profile untuk Torado Group, perusahaan di bidang food and beverage, dengan fokus pada tampilan yang menarik, responsif, dan mencerminkan identitas brand.",
+      "Created a responsive and visually appealing company profile website for Torado Group, ensuring a strong digital presence and alignment with the company's brand identity.",
     link: "https://toradogroup.com/",
   },
 ];
